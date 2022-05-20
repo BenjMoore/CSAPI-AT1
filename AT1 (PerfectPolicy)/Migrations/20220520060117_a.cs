5334cdee993aca68e721a16bfdc92302b99139be
Benjamin Moore <moorebenjamin30@gmail.com>
2022-05-20 16:02:23 +1000

    v2

---
+++ AT1 (PerfectPolicy)/Migrations/20220520060117_a.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AT1__PerfectPolicy_.Migrations
+{
+    public partial class a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 20, 16, 1, 17, 268, DateTimeKind.Local).AddTicks(2855));
+
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 20, 16, 1, 17, 269, DateTimeKind.Local).AddTicks(4234));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 13, 13, 5, 2, 800, DateTimeKind.Local).AddTicks(5592));
+
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 13, 13, 5, 2, 801, DateTimeKind.Local).AddTicks(2464));
+        }
+    }
+}
